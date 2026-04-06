e94db9e5db6a7b5343587028e2f75f96b98b3eb3
MaroquioFx <lucasmgava@gmail.com>
2026-04-06 20:56:57 -0300

    refactor: adjust ad zone and clean up duplicate service workers

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 10834825
+    "zoneId": 10836855
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
